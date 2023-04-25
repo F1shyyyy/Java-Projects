118364132cd3f3dc517af97446d4adb3c2ef8197
Ondřej Kábele <126025302+F1shyyyy@users.noreply.github.com>
2023-04-25 15:51:02 +0200

    Added some code and finished the task

--- Menu/src/menu.java
+++ Menu/src/menu.java
@@ -30,7 +30,7 @@ public class menu {
                             selectionSort();
                             break;
                         case 4:
-                            mergeSort();
+                            mergeSort(cisla);
                             break;
                         default:
                             System.out.println("Nebyl vybrán správný algoritmus.");
@@ -44,12 +44,64 @@ public class menu {
         }
     }
 
-    static void mergeSort() {
+    static void mergeSort(int[] cisla) {
+        if (cisla.length > 1) {
+            int stred = cisla.length / 2;
+            int[] levaStrana = new int[stred];
+            int[] pravaStrana = new int[cisla.length - stred];
+            for (int i = 0; i < stred; i++) {
+                levaStrana[i] = cisla[i];
+            }
+            for (int i = stred; i < cisla.length; i++) {
+                pravaStrana[i - stred] = cisla[i];
+            }
+            mergeSort(levaStrana);
+            mergeSort(pravaStrana);
+            merge(cisla, levaStrana, pravaStrana);
+            vypisPole();
+        }
+    }
 
+    static void merge(int[] cisla, int[] levaStrana, int[] pravaStrana){
+        int delkaLeva = levaStrana.length;
+        int delkaPrava = pravaStrana.length;
+        int i = 0, j = 0, k = 0;
+        while (i < delkaLeva && j < delkaPrava) {
+            if(levaStrana[i] <= pravaStrana[j]){
+                cisla[k] = levaStrana[i];
+                i++;
+            }
+            else{
+                cisla[k] = pravaStrana[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < delkaLeva) {
+            cisla[k] = levaStrana[i];
+            i++;
+            k++;
+        }
+        while (j < delkaPrava) {
+            cisla[k] = pravaStrana[j];
+            j++;
+            k++;
+        }
     }
 
     static void selectionSort() {
-
+        for (int i = 0; i < cisla.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < cisla.length; j++) {
+                if (cisla[j] < cisla[index]) {
+                    index = j;
+                }
+            }
+            int mensiCislo = cisla[index];
+            cisla[index] = cisla[i];
+            cisla[i] = mensiCislo;
+        }
+        vypisPole();
     }
 
     static void insertionSort() {
@@ -62,6 +114,7 @@ public class menu {
             }
             cisla[j] = temp;
         }
+        vypisPole();
     }
 
     static void bubbleSort() {
@@ -74,6 +127,7 @@ public class menu {
                 }
             }
         }
+        vypisPole();
     }
 
     static void najitNejmensiHodnotu() {
@@ -83,13 +137,16 @@ public class menu {
                 if (i < minimum) {
                     minimum = i;
                 }
-            }  
+            }
+            System.out.println("Nejmensi cislo je: " + minimum + "\n");
         }
     }
 
     static void pridatCislo() {
         cisla = Arrays.copyOf(cisla, cisla.length + 1);
         cisla[cisla.length - 1] = ziskatCislo("Zadej cislo: ");
+        System.out.println(Arrays.toString(cisla));
+        System.out.println();
     }
 
     static void vypisMenu(String[] polozky) {
@@ -97,7 +154,7 @@ public class menu {
         for (String polozka : polozky) {
             System.out.printf("%d. %s\n", cislo++, polozka);
         }
-        System.out.printf("%d. %s\n", 0, "Konec");
+        System.out.printf("%d. %s\n", 0, "Konec \n");
 
     }
 
@@ -107,10 +164,10 @@ public class menu {
             vypisMenu(menu);
             volba = ziskatCislo("Zadej volbu: ");
             sc.nextLine();
-            if (volba > 0 && volba <= menu.length) {
+            if (volba >= 0 && volba <= menu.length) {
                 return volba;
             }
-            System.out.println("Neplatna hodnota, zadej znovu: ");
+            System.out.println("Neplatna hodnota, zadej znovu: \n");
         } while (true);
     }
     
@@ -118,5 +175,10 @@ public class menu {
         System.out.print(zprava);
         int cislo = sc.nextInt(); sc.nextLine();
         return cislo;
+    }
+    
+    static void vypisPole(){
+        System.out.println(Arrays.toString(cisla));
+        System.out.println();
     }
 }
